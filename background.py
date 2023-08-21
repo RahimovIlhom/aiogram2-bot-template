914531f3069872458f36d1c0f68af7c8ac2cf200
Ilhomjon Rahimov <94887912+RahimovIlhom@users.noreply.github.com>
2023-08-21 11:41:46 +0500

    Create background.py

---
+++ background.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+from threading import Thread
+
+import time
+import requests
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "Rahimov Ilhomjon host"
+
+
+def run():
+    app.run(host='0.0.0.0', port=80)
+
+
+def launch():
+    t = Thread(target=run)
+    t.start()
